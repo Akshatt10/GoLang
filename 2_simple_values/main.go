ef0cc8b1877e5b4b7c33385a6ab8d94adce1b0c3
Akshatt10 <akshattyagi1010@gmail.com>
2025-06-14 10:46:42 +0530

    simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//simple values
+	// integer
+
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("hello golang")
+
+	//boolean
+	fmt.Println(true)
+
+	// float
+	fmt.Println(10.5)
+	fmt.Println((7.0 / 3.0))
+}
