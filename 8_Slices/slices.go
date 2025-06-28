c8da50d6b1e9a27a7e72fda9131523322d3a875e
Akshatt10 <akshattyagi1010@gmail.com>
2025-06-29 01:52:47 +0530

    slices_completed

--- 8_Slices/slices.go
+++ 8_Slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 //slices are dynamic arrays and most used data structure in go
 
@@ -12,8 +15,31 @@ func main() {
 	// fmt.Println(nums)
 	// fmt.Println(len(nums)) // length of slice
 
-	var nums = make([]int, 2)
+	// var nums = make([]int, 2,5)
 	//capacity the is the maximum number of elements the slice can hold
-	fmt.Println(nums, cap(nums)) // capacity of slice
-	fmt.Println(nums, len(nums)) // length of slice
+	// fmt.Println(nums, cap(nums)) // capacity of slice
+	// fmt.Println(nums, len(nums)) // length of slice
+
+	// var nums = make([]int, 0, 5)
+	// nums = append(nums, 2)
+	// var nums2 = make([]int, len(nums))
+
+	// copy(nums2, nums) //copying slice
+
+	// fmt.Println(nums, nums2) // capacity of slice
+
+	//SLICE OPERATOR
+
+	// var nums = []int{1, 2, 3}
+
+	// fmt.Println(nums[0:2]) // slicing the slice, it will return a new slice with elements from index 0 to 2 (exclusive)
+	// fmt.Println(nums[:2])  // slicing the slice, it will return a new slice with
+
+	//Slice Package
+
+	var nums = []int{1, 2, 3}
+	var num2 = []int{4, 5, 6}
+
+	fmt.Println(slices.Equal(nums, num2)) // checking if two slices are equal
+
 }
