a103e9958149fd6e80e8c0ee517dfd7e5543d4b4
Akshatt10 <akshattyagi1010@gmail.com>
2025-07-20 17:11:21 +0530

    structs

--- 12_structs/structs.go
+++ 12_structs/structs.go
@@ -1,18 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	Name := User{"Akshat", 20, true, "akshat@gmail.com"}
+	Name := User{"Akshat", 20, true, "akshat@gmail.com", time.Now()}
+	Name.createdAt = time.Now()
 	fmt.Println(Name)
 	fmt.Printf("Name of the user is %+v\n", Name.Name)
 
 }
 
 type User struct {
-	Name     string
-	Age      int
-	verified bool
-	email    string
+	Name      string
+	Age       int
+	verified  bool
+	email     string
+	createdAt time.Time
 }
